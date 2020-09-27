ab821e377583fbe0f728e574784bcfc885a23918
Rosano Coutinho <pub@xeari.com>
2020-09-27 13:13:39 -0300

    Build d7b1819

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ const mod = (function OLSKServiceWorkerModule (param1, param2, param3, param4) {
 
 			// DATA
 
-			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1601222647336',
+			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1601223206605',
 			_DataPersistenceCacheName: 'OLSKServiceWorkerPersistenceCache',
 			_DataOriginPage: '/track',
 
