e2e8e774397b56c3352f6ddfecf63fd692c5d6fb
Rosano Coutinho <pub@xeari.com>
2022-10-18 12:46:08 +0200

    Build c17191a

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@
 
 			// DATA
 
-			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1664789228922',
+			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1666089946357',
 			_DataPersistenceCacheName: 'OLSKServiceWorkerPersistenceCache',
 			_DataOriginPage: '/track',
 
